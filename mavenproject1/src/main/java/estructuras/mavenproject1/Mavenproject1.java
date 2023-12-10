5f6b02afe5ac29273db68a98810659c60ff4d489
mjlopez <ll18017@ues.edu.sv>
2023-12-10 11:08:16 -0600

    se ha creado el pool de coneciones, db para usuarios, abstractbean y bean , aun no se ha probado la PU

---
+++ mavenproject1/src/main/java/estructuras/mavenproject1/Mavenproject1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package estructuras.mavenproject1;
+
+/**
+ *
+ * @author mjlopez
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
